8cf5993075a2ebf9b39a71e167371ab3e780de5a
arvsokolov <marktwe@mail.ru>
2024-02-05 19:02:25 +0300

    Продолжаю учить Питон

---
+++ today/today.py
@@ -0,0 +1,12 @@
+today = 'Sunday'
+condition = "stomach"
+
+if today == "Saturday":
+    print("Party!")
+elif today == "Sunday":
+    if condition == "Headache":
+        print("Recover, then rest.")
+    else:
+        print("Rest")
+else:
+    print("Work, work, work!")  
