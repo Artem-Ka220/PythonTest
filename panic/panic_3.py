563cc753bc06bb4be16915e62e5b5fac7b9f138c
arvsokolov <marktwe@mail.ru>
2024-05-05 00:52:25 +0300

    Ещё работа с Panic

--- panic/panic_3.py
+++ panic/panic_3.py
@@ -3,16 +3,16 @@ plist = list(phrase)
 print(phrase)
 print(plist)
 
-plist_2 = plist[1 : len(plist) - 4 :]
+print(' ')
 
+plist_2 = plist[1 : len(plist) - 4 :]
 plist_2.pop(2)
 plist_2.insert(2, plist_2[-3])
 plist_2.pop(4)
-
-plist_2.insert(len(plist_2) - 2, plist_2[-1])
-plist_2.pop(len(plist_2) - 1)
+plist_2.insert(len(plist_2) - 2, plist_2.pop(-1))
 new_frase = ''.join(plist_2)
 
+print(' ')
 print(plist)
 print(plist_2)
 print(new_frase)
