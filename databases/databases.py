92742127b6c12134932dd35792dfba6c8e70b754
arvsokolov <marktwe@mail.ru>
2025-01-25 19:32:30 +0300

    Once again about data structures.

---
+++ databases/databases.py
@@ -0,0 +1,17 @@
+word = set()
+print(word)
+print(type(word))
+
+any_thing = dict()
+print(any_thing)
+print(type(any_thing))
+
+vowels = list()
+print(vowels)
+print(type(vowels))
+
+t = tuple()
+print(t)
+print(type(t))
+
+
